5ee6dcc8e5462485c3a93c97b3ce325aa61c22be
AdnenSahly <sahly.adnen@gmail.com>
2020-03-03 16:47:57 +0100

    second commit

---
+++ onlinebookstore/onlinebookstore/src/main/java/in/sahly/onlinebookstore/onlinebookstore/entity/BookCategorie.java
@@ -0,0 +1,31 @@
+package in.sahly.onlinebookstore.onlinebookstore.entity;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="lbl_category")
+@Setter
+@Getter
+@ToString
+public class BookCategorie {
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+@Column(name="category_name")
+private String categoryName;
+@OneToMany(cascade=CascadeType.ALL, mappedBy="category")
+private Set<Book> book;
+}
